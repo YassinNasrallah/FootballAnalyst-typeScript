da1e7d24bad2b98bffe4b46bef47dd8f6127ede5
yassin nassrallah <yassinnassrallah@gmail.com>
2026-09-02 19:15:37 +0200

    remove apikey

--- FootballAnalyse/src/libs/FootballApi.tsx
+++ FootballAnalyse/src/libs/FootballApi.tsx
@@ -3,7 +3,7 @@ import { TeamModel } from "../models/TeamModel"
 
 
 const FootballApi = () => {
- const API_key = 'bbf173f9ce928fa2fa132dc984fe75e3'
+ const API_key = 
  
  const _getplayers = async(search:string):Promise<PlayerModel[]>=>{
     const result = await fetch(`https://v3.football.api-sports.io/players/profiles?&search=${search}`,{
